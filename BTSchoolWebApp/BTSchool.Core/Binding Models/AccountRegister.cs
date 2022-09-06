bb1480f37b6763593d478e5b83e2d38155aa0475
Ilhamich <yasniy89@gmail.com>
2022-09-06 13:56:32 +0300

    Add binding models

---
+++ BTSchoolWebApp/BTSchool.Core/Binding Models/AccountRegister.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BTSchool.Core.DTOs
+{
+    public class AccountRegister : AccountCredential
+    {
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароль введен неверно")]
+        public string ConfirmPassword { get; set; }
+    }
+}
